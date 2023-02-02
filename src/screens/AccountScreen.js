5f98a59c321de6e5a40f691bc1a966bc88716cf4
ankit-wf <ankit.webframez@gmail.com>
2023-02-02 11:34:05 +0530

    Update AccountScreen.js

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,22 +1,17 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity, ImageBackground } from 'react-native'
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import BackButton from '../components/BackButton';
 import { Ionicons } from '@expo/vector-icons'
-import { Badge, Button } from 'react-native-paper';
 import { loginActions } from '../store/UserSlice'
-import * as ImagePicker from 'expo-image-picker';
-import BottomSheet from 'reanimated-bottom-sheet';
+import { useFocusEffect } from '@react-navigation/native';
 
-import RBSheet from "react-native-raw-bottom-sheet";
 
 const AccountScreen = ({ navigation }) => {
-
+  const dispatch = useDispatch();
   const storeData = useSelector(state => state.cartData.cart);
   const isLoggedIn = useSelector(state => state.userData.isLoggedIn);
   const userData = useSelector(state => state.userData.user_data);
-  // console.log("nnneeewewwwww", userData)
-
 
   useFocusEffect(
     React.useCallback(() => {
@@ -24,13 +19,8 @@ const AccountScreen = ({ navigation }) => {
         navigation.navigate('login');
       }
       return;
-
-
     }, [])
   );
-
-  const dispatch = useDispatch();
-
 
   const LogoutUserHandler = () => {
     dispatch(loginActions.loginform({ isLoggedIn: false }));
@@ -48,13 +38,9 @@ const AccountScreen = ({ navigation }) => {
       <View style={styles.Root}>
         <ImageBackground source={require('../../assets/imgBackground.png')} resizeMode='stretch' style={{ height: '100%' }}  >
           <View style={styles.profileImgRoot}>
-            <View style={{ height: 90, width: 90, borderWidth: 3, borderColor: '#CC933B', borderRadius: 100, alignSelf: 'center' }}
-
-            >
+            <View style={{ height: 90, width: 90, borderWidth: 3, borderColor: '#CC933B', borderRadius: 100, alignSelf: 'center' }}>
               <Image source={(require('../../assets/images/hair.png'))} style={styles.image} />
-
             </View>
-
             <View style={styles.profileTextRoot}>
               <View style={{ width: 300 }}>
                 <Text style={styles.redTextName}>{userData.display_name}</Text>
@@ -66,7 +52,6 @@ const AccountScreen = ({ navigation }) => {
                 <Text style={styles.redText}>{userData.user_email}</Text>
               </View>
             </View>
-
           </View>
         </ImageBackground>
       </View>
